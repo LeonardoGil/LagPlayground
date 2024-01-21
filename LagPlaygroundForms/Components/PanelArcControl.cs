998de92645a278a138ca07d5cf293c214f22fd20
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-21 00:11:16 -0300

    Panel Arc

---
+++ LagPlaygroundForms/Components/PanelArcControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace LagPlaygroundForms.Components
+{
+    public partial class PanelArcControl : Control
+    {
+        public PanelArcControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var myPath = new GraphicsPath();
+
+            var margin = 1;
+
+            var angleW = (int)(Width * 0.25) - margin;
+            var angleH = (int)(Height * 0.25) - margin;
+
+            // Left
+            myPath.StartFigure();
+            myPath.AddArc(new Rectangle(0, 0, angleW, angleH), 270, -90);
+            myPath.AddArc(new Rectangle(0, Height - angleH, angleW, angleH), 180, -90);
+            
+            myPath.AddArc(new Rectangle(Width - angleW, Height - angleH, angleW, angleH), 90, -90);
+            myPath.AddArc(new Rectangle(Width - angleW, 0, angleW, angleH), 0, -90);
+            myPath.CloseFigure();
+
+            e.Graphics.FillPath(new SolidBrush(Color.Red), myPath);
+        }
+    }
+}
---
+++ LagPlaygroundForms/Components/ShowForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace LagPlaygroundForms.Components
+{
+    partial class ShowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelArcControl1 = new PanelArcControl();
+            SuspendLayout();
+            // 
+            // panelArcControl1
+            // 
+            panelArcControl1.BackColor = Color.White;
+            panelArcControl1.Location = new Point(110, 84);
+            panelArcControl1.Name = "panelArcControl1";
+            panelArcControl1.Size = new Size(222, 300);
+            panelArcControl1.TabIndex = 0;
+            panelArcControl1.Text = "panelArcControl1";
+            // 
+            // ShowForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(573, 450);
+            Controls.Add(panelArcControl1);
+            Name = "ShowForm";
+            Text = "ShowForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PanelArcControl panelArcControl1;
+    }
+}
